1904ebc85df03ccebcc8ad47f2aadee5cf79c827
Bruno San Martin <brunosmdata@gmail.com>
2025-09-13 19:49:44 +0000

    Staging models: Adding columns, description and tests of unique and not_null

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -12,7 +12,7 @@ renamed as (
         orders_id,
         shipping_fee,
         logcost,
-        CAST (ship_cost AS FLOAT64) AS ship_coast
+        CAST (ship_cost AS FLOAT64) AS ship_cost
 
     from source
 
