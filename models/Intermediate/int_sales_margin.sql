4ebfeb6c01f6b9bb1948393cc53abcc813441c86
Bruno San Martin <brunosmdata@gmail.com>
2025-09-14 01:06:04 +0000

    Added margin per order and margin per product

--- models/Intermediate/int_sales_margin.sql
+++ models/Intermediate/int_sales_margin.sql
@@ -8,11 +8,18 @@ products AS (
     FROM {{ ref('stg_raw__product') }}
 )
 
-SELECT
-    s.*,
-    p.*,
-    s.quantity * p.purchase_price AS purchase_cost,
-    ROUND(s.revenue - (s.quantity * p.purchase_price),2) AS margin
-FROM sales s
-JOIN products p
-    ON s.products_id = p.products_id
+  SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
+
+
+    
